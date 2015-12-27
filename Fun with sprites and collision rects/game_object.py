8d035e94881c272f12571e8b08441d64a0934799
Benjamin Reed <BenjaminReed@embarqmail.com>
2015-12-26 23:40:38 -0500

    Initial commit, populating repo

---
+++ Fun with sprites and collision rects/game_object.py
@@ -0,0 +1,57 @@
+import pygame as pyg
+from spritesheet import *
+
+class GlorifiedSprite(pyg.sprite.Sprite):
+	# -------- Lists for animation frames --------
+	all_frames_R = []
+	all_frames_L = []
+	image_index = 0
+	direction = "R"
+
+	def __init__(self):
+		super(GlorifiedSprite, self).__init__()
+		self.init_frames()
+		self.image = self.all_frames_R[self.image_index]
+		self.rect = self.image.get_rect()
+		self.rect.x = 40
+		self.rect.y = 40
+		
+	def init_frames(self):
+		sprite_sheet = SpriteSheet("img/nov2015_spritesheet_2.png")
+		originX = 0
+		originY = 0
+		image_width = 120
+		image_height = 114
+		
+		for x in range (0, 27):
+			image = sprite_sheet.get_image(originX, originY, image_width, image_height)
+			self.all_frames_R.append(image)
+			originX += image_width
+			
+		originX = 0
+		for x in range (0, 27):
+			image = sprite_sheet.get_image(originX, originY, image_width, image_height)
+			image = pyg.transform.flip(image, True, False)
+			self.all_frames_L.append(image)
+			originX += image_width
+			
+	def draw(self, surface):
+		surface.blit(self.image, self.rect)
+		
+	def next_image(self):
+		self.image_index += 1
+		if self.image_index >= len(self.all_frames_R):
+			self.image_index = 0
+		if self.direction == "R":
+			self.image = self.all_frames_R[self.image_index]
+		else: 
+			self.image = self.all_frames_L[self.image_index]
+			
+	def prev_image(self):
+		self.image_index -= 1
+		if self.image_index < 0:
+			self.image_index = len(self.all_frames_R)-1
+		if self.direction == "R":
+			self.image = self.all_frames_R[self.image_index]
+		else: 
+			self.image = self.all_frames_L[self.image_index]
---
+++ Fun with sprites and collision rects/maingame.py
@@ -0,0 +1,106 @@
+"""
+Utility for testing character animation and rect members
+Written Dec 26, 2015 by Benjamin Reed
+	
+Credit for this implementation goes to Sean J. McKiernan
+(Mekire) at /r/pygame
+https://github.com/Mekire
+"""
+import sys
+
+import pygame as pyg
+import constants as con
+from game_object import *
+
+# -------- Global constants --------
+# Game window caption
+WINDOW_CAPTION = "Animation test"
+
+# Game window dimensions
+SCREEN_SIZE = (200, 200)
+
+# Target frame rate
+TARGET_FPS = 60
+
+class App:
+	"""
+	A class to cleanly encapsulate main game loop phases,
+	including initialization, event handling, and state 
+	updates
+	"""
+	debug = True
+	
+	def __init__(self):
+		"""
+		Get a reference to the display surface; set up required attributes;
+		and instantiate player objects
+		"""
+		self.screen = pyg.display.get_surface()
+		self.screen_rect = self.screen.get_rect()
+		self.clock = pyg.time.Clock()
+		self.fps = TARGET_FPS
+		self.done = False
+		self.keys = pyg.key.get_pressed()
+		# Instantiate game object members here
+		self.sprite = GlorifiedSprite()
+		
+	def event_loop(self):
+		"""
+		Method encompassing one trip through the event queue
+		Called within main_loop()
+		"""
+		for event in pyg.event.get():
+			if event.type == pyg.QUIT:
+				self.done = True
+			elif event.type in (pyg.KEYUP, pyg.KEYDOWN):
+				self.keys = pyg.key.get_pressed()
+				if event.type == pyg.KEYDOWN:
+					# Right key: Next image in list
+					if event.key == pyg.K_RIGHT:
+						self.sprite.next_image()
+					# Left key: Previous image in list
+					elif event.key == pyg.K_LEFT:
+						self.sprite.prev_image()
+				
+	def render(self):
+		"""
+		Method encompassing drawing to the screen and updating
+		the display
+		"""
+		# Wipe frame 
+		self.screen.fill(con.BG_COLOR)
+		
+		# Draw sprite
+		self.sprite.draw(self.screen)
+		
+		# Draw rects
+		if self.debug:
+			pyg.draw.rect(self.screen, con.GREEN, self.sprite.rect, 1)
+		
+		# Flip display
+		pyg.display.flip()
+		
+	def main_loop(self):
+		"""
+		Encompasses one rep of the main game loop
+		"""
+		while not self.done:
+			self.event_loop()
+			# (update game objects here)
+			self.render()
+			self.clock.tick(self.fps)
+		
+def main():
+	"""
+	Main program function. Performs Pygame initialization,
+	starts, and exits the program.
+	"""
+	pyg.init()
+	pyg.display.set_caption(WINDOW_CAPTION)
+	pyg.display.set_mode(SCREEN_SIZE)
+	App().main_loop()
+	pyg.quit()
+	sys.exit()
+	
+if __name__ == "__main__":
+	main()
